20d59e9e4a187381b6a2a16aca645531c62d1dd7
Manolo van Ee <manolo.vanee@gmail.com>
2022-04-29 10:53:46 +0200

    Update swift-nio-ssh dependency to .upToNextMinor(from: "0.4.0")

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssh.git", from: "0.3.3"),
+        .package(url: "https://github.com/apple/swift-nio-ssh.git", .upToNextMinor(from: "0.4.0")),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.1"),
     ],
     targets: [
